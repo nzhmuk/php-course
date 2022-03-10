fa3b2c1460058f4c208d69ccde8fdf1c67b5359c
Nikolay Zhmuk <nzhmuk@plesk.com>
2022-03-10 17:59:07 +0700

    Task 1, function binarySum, attempt 2

--- php-course-task-1.php
+++ php-course-task-1.php
@@ -1,9 +1,13 @@
 <?php
+/*
+ * Реализуйте функцию binarySum, которая принимает на вход два бинарных числа (в виде строк) и возвращает их сумму.
+ * Результат (вычисленная сумма) также должен быть бинарным числом в виде строки.
+*/
 
-function binarySum($binaryNum1, $binaryNum2) {
+function binarySum($binaryNum1, $binaryNum2)
+{
     $decimalNum1 = bindec($binaryNum1);
     $decimalNum2 = bindec($binaryNum2);
-    return (string)decbin($decimalNum1 + $decimalNum2);
     return decbin($decimalNum1 + $decimalNum2);
 }
 
