d126fa94c180f377723535960743fd86dcf6ac13
Nikolay Zhmuk <nzhmuk@plesk.com>
2022-03-10 17:37:53 +0700

    Task 1, function binarySum, attempt 1

--- php-course-task-1.php
+++ php-course-task-1.php
@@ -1,2 +1,11 @@
 <?php
 
+function binarySum($binaryNum1, $binaryNum2) {
+    $decimalNum1 = bindec($binaryNum1);
+    $decimalNum2 = bindec($binaryNum2);
+    return (string)decbin($decimalNum1 + $decimalNum2);
+    return decbin($decimalNum1 + $decimalNum2);
+}
+
+var_dump(binarySum('10', '1')); // 11
+var_dump(binarySum('1101', '101')); // 10010
