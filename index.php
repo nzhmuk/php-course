63a1e79e61233061d918c7ae6336c5197c578bfd
Nikolay Zhmuk <nzhmuk@plesk.com>
2022-03-25 02:05:48 +0700

    Task 3 Fibonacci, fixup

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
 <?php
+declare(strict_types=1);
+
+/*
+ * Challenge functions should be called from here
+ */
+
+$menuScreen = <<<EOT
+----------------------------------------------------------------------------------------------------------------------
+    Select a challenge:
+    1 - Challenge 1. Function binarySum(), accepts 2 binary numbers and return their sum
+    2 - Challenge 2. Function isPowerOfThree(), returns (true/false) if a given number is power of 3 or not
+    3 - Challenge 3. Function fib(), returns a positive Fibonacci number based on given index
+----------------------------------------------------------------------------------------------------------------------
+    
+EOT;
+print_r($menuScreen);
+$challengeNumber = readline("Enter number of a challenge: ");
+
+switch ($challengeNumber) {
+    case 1:
+        // binarySum
+        break;
+
+    case 2:
+        // isPowerOfThree
+        break;
+
+    case 3:
+        //fib()
+        break;
+}
