19d3787bc79804e479c1a9a61d14efe62b07fa00
Nikolay Zhmuk <nzhmuk@plesk.com>
2022-03-25 10:50:58 +0700

    Task 4. implement addDigits function

--- index.php
+++ index.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
  * Challenge functions should be called from here
  */
 
+require_once "php-course-task-1.php";
+require_once "php-course-task-2.php";
+require_once "php-course-task-3.php";
+require_once "php-course-task-4.php";
+
 $menuScreen = <<<EOT
 ----------------------------------------------------------------------------------------------------------------------
     Select a challenge:
     1 - Challenge 1. Function binarySum(), accepts 2 binary numbers and return their sum
     2 - Challenge 2. Function isPowerOfThree(), returns (true/false) if a given number is power of 3 or not
     3 - Challenge 3. Function fib(), returns a positive Fibonacci number based on given index
+    4 - Challenge 4. Function addDigits(), that shrinks argument into single number
 ----------------------------------------------------------------------------------------------------------------------
     
 EOT;
@@ -28,5 +34,13 @@ switch ($challengeNumber) {
 
     case 3:
         //fib()
+        break;
+    case 4:
+        $argument = readline("Enter a value for a function addDigits():");
+        if ($argument < 0) {
+            print_r("Value should be a positive int number");
+        } else {
+            print_r(addDigits((int)$argument));
+        }
         break;
 }
